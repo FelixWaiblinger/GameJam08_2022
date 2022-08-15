199b551c675e03f63ea85f295c5623410d285305
Felix <diebreze@gmail.com>
2022-08-15 20:25:31 +0200

    enemy 6

---
+++ GameJam/Assets/Scripts/Enemy/Enemy_6.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_6 : Enemy
+{
+    [SerializeField] private GameObject triangle;
+    [SerializeField] private float delayTime = 5f;
+
+    private float delay;
+
+    void Start()
+    {
+        FindPlayer();
+        FindEnemyTypes();
+        FindComboEvent();
+        health = 6;
+        color = new Color(73, 48, 254);
+        delay = delayTime;
+    }
+
+    void Update()
+    {
+        if (delay > 0) delay -= Time.deltaTime;
+        else
+        {
+            Instantiate(triangle, transform.position, transform.rotation);
+            delay = delayTime;
+        }
+
+        MoveToPlayer();
+    }
+}
